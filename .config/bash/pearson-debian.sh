8e07e5585bf09dd3ae635bdf586ce03447281d01
Chandler Swift <chandler+pearson@chandlerswift.com>
2020-02-12 16:06:51 -0600

    Update fix_display function

--- .config/bash/pearson-debian.sh
+++ .config/bash/pearson-debian.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 function fix_display() {
-	for i in 1 2; do
-		for mode in 1024x768 1920x1200; do
-			xrandr --output VGA-$i --mode $mode
-		done
-	done
+	xrandr --output VGA-1 --mode 1024x768
+	xrandr --output VGA-2 --mode 1024x768
+	xrandr --output VGA-1 --mode 1920x1200
+	xrandr --output VGA-2 --mode 1920x1200 --right-of VGA-1
 }
